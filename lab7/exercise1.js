134509470224372b073f8286f35ef7666f030343
fikerte-b <fikirbenti@gmail.com>
2022-05-11 15:53:54 -0500

    lab7

---
+++ lab7/exercise1.js
@@ -0,0 +1,23 @@
+isPrime = function(num){
+  return new Promise(function (resolve, reject){
+      setTimeout(() => {
+        for(let i = 2, s = Math.sqrt(num); i <= s; i++){
+        if(num % i === 0)
+         reject({prime: false});
+        }
+        resolve({prime: true});
+      }, 500);
+  });
+}
+
+
+console.log('start');
+isPrime(7)
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+console.log('end');
+
+// Output:
+// start
+// end
+// { prime: true }
